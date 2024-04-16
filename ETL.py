bff196951e6fa60ac597e6d64799a22dfc90f2be
jalisastewart <jalisastewart55@gmail.com>
2024-04-16 14:29:17 -0500

    Read CSV file to panda DataFrame

--- ETL.py
+++ ETL.py
@@ -1,4 +1,9 @@
 # Import libraries 
-import requests
-import pandas
-import sqlalchemy
+import requests # https requests
+import pandas as pd #create DataFrame
+import sqlalchemy # Create the SQLAlchemy engine
+
+# Read CSV file to panda DataFrame
+csv_file = 'C:\Users\jstew\DE_projects\BrainTree_SQL_Coding_Challenge\BrainTree_SQL_Coding_Challenge_SQL_Developer\data_csv\continent_map.csv'
+df = pd.read_csv(csv_file)
+
